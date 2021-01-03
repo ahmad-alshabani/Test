2935f67663b33494fdf1395bacc551f8b88e049d
ahmad-alshabani <ahmad-alshabani@users.noreply.github.com>
2021-01-03 19:03:12 +0300

    First Commit
    Comment

---
+++ Rogue Shadows/Assets/Gun.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100;
+    public float fireRate = 15f;
+    public float impactForce = 30f;
+
+    public int maxAmmo = 10;
+    private int gunAmmo = 10;
+    public float reloadTime = 1.5f;
+    private bool isReloading = false;
+
+    public Text ammoDisplay;
+
+    public ParticleSystem muzzleFlash;
+    public GameObject fpsCam;
+    public GameObject impactEffect;
+    AudioSource gunAudioSource;
+
+    private float nextTimeToFire = 0f;
+
+    public Animator animator;
+
+    [SerializeField] AudioClip gunShot;
+    [SerializeField] AudioClip gunReload;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        if (gunAmmo == -1)
+        {
+            gunAmmo = maxAmmo;
+        }
+        gunAudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
+
+    void Update()
+    {
+        if (isReloading)
+            return;
+
+        if (gunAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+        ammoDisplay.text = "Ammo: " + gunAmmo.ToString();
+
+    }
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        gunAudioSource.PlayOneShot(gunReload);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        gunAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+    void Shoot()
+    {
+        if (gunAmmo != 0f)
+        {
+            gunAmmo--;
+            muzzleFlash.Play();
+            gunAudioSource.PlayOneShot(gunShot);
+            RaycastHit hit;
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
+
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
+            }
+        }
+    }
+
+}
+
---
+++ Rogue Shadows/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController character;
+
+    public float speed = 12f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    public bool isGrounded;
+
+    AudioSource playerFX;
+    [SerializeField] AudioClip footSteps;
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        character = GetComponent<CharacterController>();
+        playerFX = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = speed +2f;
+        }
+        else
+        {
+            speed = 12f;
+        }
+        character.Move(move * speed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            if (!playerFX.isPlaying)
+            {
+                playerFX.PlayOneShot(footSteps);
+            }
+        }
+        else
+        {
+            playerFX.Stop();
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        character.Move(velocity * Time.deltaTime);
+
+    }
+}
